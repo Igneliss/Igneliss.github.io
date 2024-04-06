278142f345ddabf7c08765d8c5379cb589dad0ea
Igneliss <65406788+Igneliss@users.noreply.github.com>
2024-04-06 15:34:52 +0300

    Delete scriptportfolio.js

--- scriptportfolio.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  document.body.style.opacity = "1";
-}
-const links = document.getElementById('links');
-const newLink = document.createElement('li');
-newLink.appendChild(newLinkText);
-links.appendChild(newLink);
